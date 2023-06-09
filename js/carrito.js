5f2be95db51d0bdf142d8d571e93db0195a71b6d
juanandreacchio <juan.andreacchio@gmail.com>
2023-06-09 13:33:49 -0300

    Finalizar compra

--- js/carrito.js
+++ js/carrito.js
@@ -12,65 +12,80 @@ let botonesRestarCantidad = document.querySelectorAll('.restarCantidadProducto')
 const containerAccionesCarrito = document.querySelector('.acciones-carito');
 let total = document.querySelector('#total');
 const botonVaciar = document.querySelector('#vaciarCarrito');
+const tituloNumeroTarjeta = document.querySelector('.tarjeta');
+const inputTarjeta = document.querySelector('.numberTarjeta');
+var DateTime = luxon.DateTime;
+let fechasDeLlegada = document.querySelector('.llegada');
+const btnFinalizarCompra = document.querySelector('.finalizarCompra')
+const containerCarrito = document.querySelector('.carrito');
+const botonComprar = document.querySelector('.comprar');
+const inputCVV = document.querySelector('.codigoDeSeguridad')
+const inputFechaVencimiento = document.querySelector('.dateVencimiento');
+const inputDireccion = document.querySelector('#inputDireccion')
+const inputNumeroDireccion = document.querySelector('#inputNumeroDireccion');
+const inputCP = document.querySelector('#inputCP');
+const mensajeDeError = document.querySelector('.mensajeDeError');
+const containerEnvio = document.querySelector('.envio');
+const containerTarjeta = document.querySelector('.medioDePago');
+const containerDireccion = document.querySelector('.direccion');
+const containerBtnComprar = document.querySelector('.botonComprar');
+const btnVolver = document.querySelector('.volver')
 
-console.log(productosEnCarrito);
 
 
+ function cargarProductosAlCarro(){
+   if(productosEnCarrito && productosEnCarrito.length > 0){
+     carritoVacio.classList.add('none');
+     containerProductos.classList.remove('none');
+     containerAccionesCarrito.classList.remove('none');
+     containerProductos.innerHTML = '';
+     productosEnCarrito.forEach(producto => {
+         const div = document.createElement("div");
+         div.classList.add("productoCarro");
+         div.innerHTML = `
+           <img src="${producto.imagen}" alt="${producto.titulo}" srcset="" style="width: 85px; height: 85px;">
+           <div class="descripcionProductoCarro">
+             <small>Producto</small>
+             <p>
+               ${producto.titulo}
+             </p>
+           </div>
+           <div class="cantidadProductoCarro">
+             <small>Cantidad</small>
+             <p>
+               <button class="restarCantidadProducto" id="${producto.id}">-</button> ${producto.cantidad} <button class="aumentarCantidadProducto" id="${producto.id}">+</button>
+             </p>
+           </div>
+           <div class="precioProductoCarro">
+             <small>Precio</small>
+             <p>$${(producto.precio).toFixed(2)}</p>
+           </div>
+           <div class="subtotal">
+             <small>Subtotal</small>
+             <p>
+               $${(producto.precio * producto.cantidad).toFixed(2)}
+             </p>
+           </div>
+           <button class="eliminarProductoCarro" id="${producto.id}">
+             <i class="fa-solid fa-trash"></i>
+           </button>
+         `
+         containerProductos.append(div)
+     });
+     actualizaBotonesDeEliminar();
+     actualizarBotonesDisminuir();
+     actualizarBotonesAumentar();
+     actualizarTotal();
+   }
+   else{
+     carritoVacio.classList.remove('none');
+     containerProductos.classList.add('none');
+     containerAccionesCarrito.classList.add('none');
+   }
 
+ }
 
-function cargarProductosAlCarro(){
-  if(productosEnCarrito && productosEnCarrito.length > 0){
-    carritoVacio.classList.add('none');
-    containerProductos.classList.remove('none');
-    containerAccionesCarrito.classList.remove('none');
-    containerProductos.innerHTML = '';
-    productosEnCarrito.forEach(producto => {
-        const div = document.createElement("div");
-        div.classList.add("productoCarro");
-        div.innerHTML = `
-          <img src="${producto.imagen}" alt="${producto.titulo}" srcset="" style="width: 85px; height: 85px;">
-          <div class="descripcionProductoCarro">
-            <small>Producto</small>
-            <p>
-              ${producto.titulo}
-            </p>
-          </div>
-          <div class="cantidadProductoCarro">
-            <small>Cantidad</small>
-            <p>
-              <button class="restarCantidadProducto" id="${producto.id}">-</button> ${producto.cantidad} <button class="aumentarCantidadProducto" id="${producto.id}">+</button>
-            </p>
-          </div>
-          <div class="precioProductoCarro">
-            <small>Precio</small>
-            <p>$${(producto.precio).toFixed(2)}</p>
-          </div>
-          <div class="subtotal">
-            <small>Subtotal</small>
-            <p>
-              $${(producto.precio * producto.cantidad).toFixed(2)}
-            </p>
-          </div>
-          <button class="eliminarProductoCarro" id="${producto.id}">
-            <i class="fa-solid fa-trash"></i>
-          </button>
-        `
-        containerProductos.append(div)
-    });
-    actualizaBotonesDeEliminar();
-    actualizarBotonesDisminuir();
-    actualizarBotonesAumentar();
-    actualizarTotal();
-  }
-  else{
-    carritoVacio.classList.remove('none');
-    containerProductos.classList.add('none');
-    containerAccionesCarrito.classList.add('none');
-  }
-
-}
-
-cargarProductosAlCarro();
+ cargarProductosAlCarro();
 
 
 !darkMode && localStorage.setItem('dark-mode',"desactivado");
@@ -89,120 +104,221 @@ function desactivarDarkMode(){
     bodyDoc.classList.remove('dark-mode');
 }
 
-console.log(botonCambiarMode.innerHTML);
+ console.log(botonCambiarMode.innerHTML);
 
-function actualizaBotonesDeEliminar (){
-  botonesEliminar = document.querySelectorAll('.eliminarProductoCarro');
-  botonesEliminar.forEach(boton =>{
-    boton.addEventListener('click',eliminarDelCarrito);
-  });
-}
+ function actualizaBotonesDeEliminar (){
+   botonesEliminar = document.querySelectorAll('.eliminarProductoCarro');
+   botonesEliminar.forEach(boton =>{
+     boton.addEventListener('click',eliminarDelCarrito);
+   });
+ }
 
-function actualizarTotal(){
-  let precioTotal = productosEnCarrito.reduce((acc,producto) => acc + (producto.precio * producto.cantidad),0)
-  total.innerText = `Total: $${precioTotal.toFixed(2)}`
-}
+ function actualizarTotal(){
+   let precioTotal = productosEnCarrito.reduce((acc,producto) => acc + (producto.precio * producto.cantidad),0)
+   total.innerText = `Total: $${precioTotal.toFixed(2)}`
+ }
 
-function actualizarBotonesDisminuir(){
-  botonesRestarCantidad = document.querySelectorAll('.restarCantidadProducto');
-  botonesRestarCantidad.forEach(boton =>{
-    boton.addEventListener('click',disminuirCantidad);
-  })
-}
+ function actualizarBotonesDisminuir(){
+   botonesRestarCantidad = document.querySelectorAll('.restarCantidadProducto');
+   botonesRestarCantidad.forEach(boton =>{
+     boton.addEventListener('click',disminuirCantidad);
+   })
+ }
 
-function actualizarBotonesAumentar(){
-  botonesAumentarCantidad = document.querySelectorAll('.aumentarCantidadProducto');
-  botonesAumentarCantidad.forEach(boton =>{
-    boton.addEventListener('click',aumentarCantidad);
-  })
-}
+ function actualizarBotonesAumentar(){
+   botonesAumentarCantidad = document.querySelectorAll('.aumentarCantidadProducto');
+   botonesAumentarCantidad.forEach(boton =>{
+     boton.addEventListener('click',aumentarCantidad);
+   })
+ }
 
-function aumentarCantidad (e){
-  const idBoton = e.currentTarget.id;
-  console.log(idBoton);
-  const indice = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-  productosEnCarrito[indice].cantidad++;
-  cargarProductosAlCarro();
-  localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
-}
+ function aumentarCantidad (e){
+   const idBoton = e.currentTarget.id;
+   console.log(idBoton);
+   const indice = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+   productosEnCarrito[indice].cantidad++;
+   cargarProductosAlCarro();
+   localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+ }
 
-function disminuirCantidad (e){
-  const idBoton = e.currentTarget.id;
-  console.log(idBoton);
-  const indice = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-  productosEnCarrito[indice].cantidad = productosEnCarrito[indice].cantidad - 1;
-  productosEnCarrito[indice].cantidad === 0 && productosEnCarrito.splice(indice,1);
-  cargarProductosAlCarro();
-  localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
-}
+ function disminuirCantidad (e){
+   const idBoton = e.currentTarget.id;
+   console.log(idBoton);
+   const indice = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+   productosEnCarrito[indice].cantidad = productosEnCarrito[indice].cantidad - 1;
+   productosEnCarrito[indice].cantidad === 0 && productosEnCarrito.splice(indice,1);
+   cargarProductosAlCarro();
+   localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+ }
 
-function eliminarDelCarrito(e){
-  const idBoton = e.currentTarget.id;
-  const indice = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-  productosEnCarrito.splice(indice,1);
-  cargarProductosAlCarro();
-  localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
-}
+ function eliminarDelCarrito(e){
+   const idBoton = e.currentTarget.id;
+   const indice = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+   productosEnCarrito.splice(indice,1);
+   cargarProductosAlCarro();
+   localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+ }
 
-botonVaciar.addEventListener('click',vaciarCarrito);
+ botonVaciar.addEventListener('click',vaciarCarrito);
 
-function vaciarCarrito(){
-  const cantidadDeProductos = productosEnCarrito.reduce((acc,producto) => acc + producto.cantidad,0);
-  darkMode = localStorage.getItem('dark-mode');
-  if(darkMode === "desactivado"){
-  Swal.fire({
-    title: '¿Estás seguro?',
-    text: `¿Deseas eliminar los ${cantidadDeProductos} productos del carrito? `,
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Si!',
-    cancelButtonText: 'Cancelar',
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Vaciado!',
-        'El carrito fue vaciado correctamente.',
-        'success',
-        productosEnCarrito = [],
-        cargarProductosAlCarro(),
-        localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito))
-      )
-    }
-  })
-  }
-  else{
+ function vaciarCarrito(){
+   const cantidadDeProductos = productosEnCarrito.reduce((acc,producto) => acc + producto.cantidad,0);
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === "desactivado"){
+   Swal.fire({
+     title: '¿Estás seguro?',
+     text: `¿Deseas eliminar los ${cantidadDeProductos} productos del carrito? `,
+     icon: 'warning',
+     showCancelButton: true,
+     confirmButtonColor: '#3085d6',
+     cancelButtonColor: '#d33',
+     confirmButtonText: 'Si!',
+     cancelButtonText: 'Cancelar',
+   }).then((result) => {
+     if (result.isConfirmed) {
+       Swal.fire(
+         'Vaciado!',
+         'El carrito fue vaciado correctamente.',
+         'success',
+         productosEnCarrito = [],
+         cargarProductosAlCarro(),
+         localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito))
+       )
+     }
+   })
+   }
+   else{
 
-    Swal.fire({
-      title: '¿Estás seguro?',
-      text: `¿Deseas eliminar los ${cantidadDeProductos} productos del carrito? `,
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si!',
-      cancelButtonText: 'Cancelar',
-      iconColor: "#FF0035",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Vaciado!',
-          'El carrito fue vaciado correctamente.',
-          'success',
-          productosEnCarrito = [],
-          cargarProductosAlCarro(),
-          localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito))
-        )
-      }
-    })
-  }
-}
+     Swal.fire({
+       title: '¿Estás seguro?',
+       text: `¿Deseas eliminar los ${cantidadDeProductos} productos del carrito? `,
+       icon: 'warning',
+       showCancelButton: true,
+       confirmButtonColor: '#3085d6',
+       cancelButtonColor: '#d33',
+       confirmButtonText: 'Si!',
+       cancelButtonText: 'Cancelar',
+       iconColor: "#FF0035",
+     }).then((result) => {
+       if (result.isConfirmed) {
+         Swal.fire(
+           'Vaciado!',
+           'El carrito fue vaciado correctamente.',
+           'success',
+           productosEnCarrito = [],
+           cargarProductosAlCarro(),
+           localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito))
+         )
+       }
+     })
+   }
+ }
 
 botonCambiarMode.addEventListener('click', () =>{
     darkMode = localStorage.getItem('dark-mode');
     darkMode === "activado" && (desactivarDarkMode(),botonCambiarMode.innerHTML = '<i class="fa-solid fa-sun"></i>')
     darkMode === "desactivado" && (activarDarkMode(),botonCambiarMode.innerHTML = '<i class="fa-solid fa-moon"></i>');
 });
+
+btnFinalizarCompra.addEventListener('click', () =>{
+
+
+
+
+carritoVacio.classList.add('none');
+containerProductos.classList.add('none');
+containerAccionesCarrito.classList.add('none');
+containerEnvio.classList.remove('none');
+containerTarjeta.classList.remove('none');
+containerDireccion.classList.remove('none');
+containerBtnComprar.classList.remove('none');
+var cleave = new Cleave('.codigoDeSeguridad', {
+  blocks: [4],
+  uppercase: true
+});
+
+btnVolver.addEventListener('click',() =>{
+cargarProductosAlCarro();
+containerEnvio.classList.add('none');
+containerTarjeta.classList.add('none');
+containerDireccion.classList.add('none');
+containerBtnComprar.classList.add('none');
+})
+
+var cleave = new Cleave('.numberTarjeta', {
+  creditCard: true,
+  onCreditCardTypeChanged: function (type) {
+    switch(type){
+      case 'mastercard':
+        tituloNumeroTarjeta.innerHTML = 'Número de tarjeta <i class="fa-brands fa-cc-mastercard fa-xl"></i>'
+        break;
+      case 'visa':
+        tituloNumeroTarjeta.innerHTML = 'Número de tarjeta <i class="fa-brands fa-cc-visa fa-xl"></i>'
+        break;
+      case 'amex':
+        tituloNumeroTarjeta.innerHTML = 'Número de tarjeta <i class="fa-brands fa-cc-amex fa-xl"></i>'
+        break;
+      case 'jcb':
+        tituloNumeroTarjeta.innerHTML = 'Número de tarjeta <i class="fa-brands fa-cc-jcb fa-xl"></i>'
+        break;
+      case 'discover':
+        tituloNumeroTarjeta.innerHTML = 'Número de tarjeta <i class="fa-brands fa-cc-discover fa-xl"></i>">'
+        break;
+      default:
+        tituloNumeroTarjeta.innerHTML = 'Número de tarjeta';
+        break;
+  }
+}
+});
+
+const fechaActual = DateTime.local();
+const fechaEnDosDias = fechaActual.plus({ days: 2 });
+const dia2Dias = fechaEnDosDias.day;
+const month2Dias = fechaEnDosDias.toLocaleString({ month: "long" });
+const fechaEnCincoDias = fechaActual.plus({ days: 5 });
+const dia5Dias = fechaEnCincoDias.day;
+const month5Dias = fechaEnCincoDias.toLocaleString({ month: "long" });
+
+fechasDeLlegada.innerText = `Su compra llegará entre el ${dia2Dias} de ${month2Dias} y ${dia5Dias} de ${month5Dias}`;
+
+var cleave = new Cleave('.dateVencimiento', {
+  date: true,
+  datePattern: ['m', 'y']
+});
+
+botonComprar.addEventListener('click',() =>{
+  if (inputTarjeta.value === "" || inputCVV.value.length < 3 || inputTarjeta.value.length < 15 || inputFechaVencimiento.value === "" || inputCP === "" || inputDireccion === "" || inputCVV === ""){
+    mensajeDeError.classList.remove('none')
+  }
+  else{
+    productosEnCarrito = [],
+    cargarProductosAlCarro(),
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+    carritoVacio.classList.remove('none');
+    containerEnvio.classList.add('none');
+containerTarjeta.classList.add('none');
+containerDireccion.classList.add('none');
+containerBtnComprar.classList.add('none');
+Swal.fire({
+  icon: 'success',
+  title: 'Felicidades!',
+  text: 'Compra realizada con éxito! ',
+  footer: '<a href="../index.html">Seguir comprando</a>'
+})
+  }
+})
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
--- js/registro.js
+++ js/registro.js
@@ -19,6 +19,39 @@ btnRegistrarse.addEventListener('click',() =>{
         baseDeUsuarios.push(nuevoUser);
         localStorage.setItem("Base-de-datos",JSON.stringify(baseDeUsuarios));
         console.log(baseDeUsuarios);
+        darkMode = localStorage.getItem('dark-mode');
+        if(darkMode === "desactivado"){
+            Toastify({
+                text: "Cuenta registrada!",
+                duration: 1000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "linear-gradient(90deg, rgba(104,197,219,1) 0%, rgba(95,168,208,1) 31%, rgba(202,233,255,1) 100%)",
+                  color: "#101D42"
+                },
+                onClick: function(){} // Callback after click
+              }).showToast();
+            }
+            else{
+                Toastify({
+                    text: "Cuenta registrada!",
+                    duration: 1000,
+                    destination: "https://github.com/apvarun/toastify-js",
+                    newWindow: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "right", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                      background: "linear-gradient(90deg, rgba(74,43,92,1) 0%, rgba(61,42,85,1) 31%, rgba(97,61,193,1) 100%)",
+                      color: "white"
+                    },
+                    onClick: function(){} // Callback after click
+                  }).showToast();
+            }
         inputPass.value = "";
         inputUsuario.value = "";
     }
